baa8d7263a3d4a607c959427c12619b68d2d6ca6
MarwaMohamed <marwam.2@hotmail.com>
2020-11-14 21:12:59 -0600

    updating some files and unit test cases

--- boundingmain.py
+++ boundingmain.py
@@ -1,8 +1,28 @@
-import bounding
-from bounding import BoundingBoxStraightLine
+import cgi
+import re
 
 # the function starts
-def findTheSlope(X, Y):
+def findTheSlope(bottomLeftX, bottomLeftY, topRightX, topRightY):
+
+        # Here I am testing that the type error is handled
+        list = [bottomLeftX, bottomLeftY, topRightX, topRightY]
+        for l in list:
+            if type(l) is str and re.match(r'^-?\d+(?:\.\d+)?$', l) is None:  # Here only strings which can be numbers are accepted
+                raise TypeError("only real coordinates number like 1 or 1.0 or 1.4 ..etc are acceptable")
+            if type(l) is not float and type(l) is not int and type(l) is not str:
+                raise TypeError("only real coordinates number like 1 or 1.0 or 1.4 ..etc are acceptable")
+
+
+
+        # When checking is ok
+        SbottomLeftX = float(bottomLeftX)
+        SbottomLeftY = float(bottomLeftY)
+        StopRightX = float(topRightX)
+        StopRightY = float(topRightY)
+        X = StopRightX - SbottomLeftX
+        Y = StopRightY - SbottomLeftY
+
+        # Checking that the user draw a box not a straight line
         if Y == 0:
             raise ValueError("Vertical straight lines aren't boxes")
         if X == 0:
@@ -13,8 +33,20 @@ def findTheSlope(X, Y):
 # the function ends
 
 # the actual code starts
-straightLineBox = BoundingBoxStraightLine(0, 1, 3, 1)
-result = findTheSlope(straightLineBox.getDeltaX(), straightLineBox.getDeltaY())
+# #!/usr/bin/python
+# print ("Content-type:text/html\r\n\r\n")
+# form = cgi.FieldStorage()
+# bottomLeftX = form.getvalue('bottomLeftX')
+# bottomLeftY  = form.getvalue('bottomLeftY')
+# topRightX = form.getvalue('topRightX')
+# topRightY= form.getvalue('topRightY')
+#
+# print(bottomLeftX)
+# print(bottomLeftY)
+# print(topRightX)
+# print(topRightY)
+
+result = findTheSlope(0,1,3,7)
 print(result) #justForTesting
 
 #the actual code ends
--- tests/test_findTheSlope.py
+++ tests/test_findTheSlope.py
@@ -5,19 +5,26 @@ from boundingmain import findTheSlope
 class TestTheStraightLineSlope(unittest.TestCase):
     def test_validArguments(self):
         # Test that both deltaX and deltaY > 0
-        self.assertEqual(findTheSlope(3, 6), 2)
-        self.assertEqual(findTheSlope(3.0, 6), 2)
-        self.assertEqual(findTheSlope(3, 6.0), 2)
-        self.assertEqual(findTheSlope(3.0, 6.0), 2)
-        self.assertEqual(findTheSlope(3, 6), 2.0)
-        self.assertEqual(findTheSlope(3.0, 6), 2.0)
-        self.assertEqual(findTheSlope(3, 6.0), 2.0)
-        self.assertEqual(findTheSlope(3.0, 6.0), 2.0)
+        self.assertEqual(findTheSlope(0,1,3,7), 2)
+        self.assertEqual(findTheSlope(0.0,1,3,7), 2)
+        self.assertEqual(findTheSlope(0,1.0,3,7), 2)
+        self.assertEqual(findTheSlope(0,1,3.0,7), 2)
+        self.assertEqual(findTheSlope(0, 1, 3, 7.0), 2)
+        self.assertEqual(findTheSlope(0.0, 1.0, 3.0, 7.0), 2)
+        self.assertEqual(findTheSlope(0,1,3,7), 2.0)
+        self.assertEqual(findTheSlope(0.0,1,3,7), 2.0)
+        self.assertEqual(findTheSlope(0,1.0,3,7), 2.0)
+        self.assertEqual(findTheSlope(0,1,3.0,7), 2.0)
+        self.assertEqual(findTheSlope(0, 1, 3, 7.0), 2.0)
+        self.assertEqual(findTheSlope(0.0, 1.0, 3.0, 7.0), 2.0)
 
     def test_InvalidArguments(self):
         # Test that both deltaX and deltaY > 0
-        self.assertRaises(ZeroDivisionError, findTheSlope, 0, 3)
-        self.assertRaises(ValueError, findTheSlope, 3, 0)
+        self.assertRaises(ZeroDivisionError, findTheSlope, 3,1,3,7)
+        self.assertRaises(ValueError, findTheSlope,0,1 ,3, 1)
+        self.assertRaises(TypeError, findTheSlope, [0, 8], 1, 3, 7)
+        self.assertRaises(TypeError, findTheSlope, "hello", 1, 3, 7)
+        self.assertRaises(TypeError, findTheSlope, "0.o", 1, 3, 7)
 
 
 
