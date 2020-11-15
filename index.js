142dc100eda960937430e28d1fc60b52e25f8d4f
MarwaMohamed <marwam.2@hotmail.com>
2020-11-15 14:03:01 -0600

    checking the file extension if it is image or not

--- index.js
+++ index.js
@@ -2,23 +2,29 @@
 const fileInput = document.getElementById("imageFile"),
       imgCanvas = document.getElementById("img"),
       imgContext = imgCanvas.getContext("2d");
+/* rendering the chosen image in canvas*/
 
+fileInput.addEventListener('change', event => {
+    // FILE NAME AND EXTENSION.
+    const fileName = fileInput.files.item(0).name;
+    fileExtension = fileName.replace(/^.*\./, '');
 
-/* rendering the choosen image in canvas*/
-
-fileInput.addEventListener('change', e => {
-    const reader = new FileReader(),
-          image = new Image(); 
-    reader.onload = e => {
-        image.src = e.target.result;
-        image.onload = function() {
-            imgCanvas.width = image.width;
-            imgCanvas.height = image.height;
-            imgContext.drawImage(image, 0, 0);
-        };
+    if (fileExtension == 'png' || fileExtension == 'jpg' || fileExtension == 'jpeg' || fileExtension == 'apng' || fileExtension == 'avif'|| fileExtension == 'gif'|| fileExtension == 'jfif'
+        || fileExtension == 'pjpeg' || fileExtension == 'pjp' || fileExtension == 'svg' || fileExtension == 'webp' || fileExtension == 'bmp' || fileExtension == 'ico' || fileExtension == 'cur'
+        || fileExtension == 'tif' || fileExtension == 'tiff') {
+        const reader = new FileReader(),
+            image = new Image();
+        reader.onload = e => {
+            image.src = e.target.result;
+            image.onload = function() {
+                imgCanvas.width = image.width;
+                imgCanvas.height = image.height;
+                imgContext.drawImage(image, 0, 0);
+            };
+        }
+        const file = event.target.files[0];
+        reader.readAsDataURL(file);
+    } else {
+        alert("Only images are accepted");
     }
-    
-    const f = e.target.files[0];
-    reader.readAsDataURL(f);
-  
 })
