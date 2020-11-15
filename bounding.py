1af822b283ee430c5c95db685c30250653506aa8
MarwaMohamed <marwam.2@hotmail.com>
2020-11-14 19:28:16 -0600

    Some test cases are added to the boundingboxstraightline class and it's unit test

--- bounding.py
+++ bounding.py
@@ -1,14 +1,25 @@
-
+import re
 # Beginning of the BoundingBoxStraightLine class
 class BoundingBoxStraightLine:
 
     def __init__(self, bottomLeftX, bottomLeftY, topRightX, topRightY):
-        self.bottomLeftX = bottomLeftX
-        self.bottomLeftY = bottomLeftY
-        self.topRightX = topRightX
-        self.topRightY = topRightY
-        self.X = 0
-        self.Y = 0
+        # Here I am testing that the type error is handled
+
+        list = [bottomLeftX, bottomLeftY, topRightX, topRightY]
+        for l in list:
+            if type(l) is not float and type(l) is not int and type(l) is not str:
+                raise TypeError("only real coordinates number like 1 or 1.0 or 1.4 ..etc are acceptable")
+
+            if type(l) is str and re.match(r'^-?\d+(?:\.\d+)?$', l) is None: # Here only strings which can be numbers are accepted
+                raise TypeError("only real coordinates number like 1 or 1.0 or 1.4 ..etc are acceptable")
+
+        # After every thing is okay
+        self.bottomLeftX = float(bottomLeftX)
+        self.bottomLeftY = float(bottomLeftY)
+        self.topRightX = float(topRightX)
+        self.topRightY = float(topRightY)
+        self.X = 0.0
+        self.Y = 0.0
         self.calculateTheDeltas()
 
 
--- tests/test_boundingboxStraightLine.py
+++ tests/test_boundingboxStraightLine.py
@@ -1,8 +1,15 @@
 import unittest
+from bounding import *
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_InvalidInput(self):
+        # self.assertRaises(Error, BoundingBoxStraightLine([0, 1],3,7))
+        self.assertRaises(TypeError, BoundingBoxStraightLine,[0,8],1, 3, 7)
+        self.assertRaises(TypeError, BoundingBoxStraightLine,"hello", 1, 3, 7)
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
